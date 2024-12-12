f3e2009a7a914efb76cd2b7b39feecdbf09ffbd9
Actually IAmPlayer <70600266+NotIAmPlayer@users.noreply.github.com>
2024-12-12 08:41:26 +0700

    get meeting by id

--- main.go
+++ main.go
@@ -1,36 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type meeting struct {
+	ID          string   `json:"id"`
+	MeetingFrom string   `json:"meeting_from"`
+	MeetingTo   []string `json:"meeting_to"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Date        string   `json:"date"`
+	StartTime   string   `json:"start_time"`
+	EndTime     string   `json:"end_time"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var meetings = []meeting{}
+
+func getMeetings(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, meetings)
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getMeetingByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, m := range meetings {
+		if m.ID == id {
+			c.IndentedJSON(http.StatusOK, m)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "meetings not found"})
+}
+
+func postMeetings(c *gin.Context) {
+	var newMeeting meeting
+
+	if err := c.BindJSON(&newMeeting); err != nil {
+		return
+	}
+
+	fmt.Println("POSTED!")
+
+	meetings = append(meetings, newMeeting)
+	c.IndentedJSON(http.StatusCreated, newMeeting)
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	/*
+		r.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
 		})
-	})
-	r.GET("/albums", getAlbums)
+	*/
+
+	r.GET("/meetings", getMeetings)
+	r.GET("/meetings/:id", getMeetingByID)
+	r.POST("/meetings", postMeetings)
 
 	r.Run("localhost:8080")
 }
